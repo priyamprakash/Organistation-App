ca95ed2a78be5472409fe6ae3464b91163955ca1
Priyam Prakash <priyamprakash99@gmail.com>
2021-11-07 03:30:01 +0530

    xml Design for adding faculty

---
+++ app/src/main/java/com/app/adminorganisation/Faculty/AddTeacher.java
@@ -0,0 +1,16 @@
+package com.app.adminorganisation.Faculty;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.app.adminorganisation.R;
+
+public class AddTeacher extends AppCompatActivity {
+    
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_teacher);
+    }
+}
--- app/src/main/java/com/app/adminorganisation/MainActivity.java
+++ app/src/main/java/com/app/adminorganisation/MainActivity.java
@@ -7,8 +7,11 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 
-public class MainActivity extends AppCompatActivity implements View.OnClickListener{
-CardView uploadNotice,addGalleryImage,addEbook;
+import com.app.adminorganisation.Faculty.UpdateFaculty;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+    CardView uploadNotice, addGalleryImage, addEbook, faculty;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -16,31 +19,37 @@ CardView uploadNotice,addGalleryImage,addEbook;
         uploadNotice = findViewById(R.id.addNotice);
         addGalleryImage = findViewById(R.id.addGalleryImage);
         addEbook = findViewById(R.id.addEbook);
+        faculty = findViewById(R.id.faculty);
 
         uploadNotice.setOnClickListener(this);
         addGalleryImage.setOnClickListener(this);
         addEbook.setOnClickListener(this);
+        faculty.setOnClickListener(this);
 
-            
+
     }
 
     @Override
     public void onClick(View view) {
         Intent intent;
-        switch (view.getId()){
+        switch (view.getId()) {
             case R.id.addNotice:
-                 intent = new Intent(MainActivity.this , UploadNotice.class);
+                intent = new Intent(MainActivity.this, UploadNotice.class);
                 startActivity(intent);
                 break;
             case R.id.addGalleryImage:
-                 intent = new Intent(MainActivity.this , UploadImage.class);
+                intent = new Intent(MainActivity.this, UploadImage.class);
                 startActivity(intent);
                 break;
-                case R.id.addEbook:
-                 intent = new Intent(MainActivity.this , UploadPdfActivity.class);
+            case R.id.addEbook:
+                intent = new Intent(MainActivity.this, UploadPdfActivity.class);
                 startActivity(intent);
                 break;
-                
+            case R.id.faculty:
+                intent = new Intent(MainActivity.this, UpdateFaculty.class);
+                startActivity(intent);
+                break;
+
         }
     }
 }
