42b8cf64b1cf689268a6503d91b6e12c3ad7c193
Priyam Prakash <priyamprakash99@gmail.com>
2021-11-09 03:20:45 +0530

    Added new Fragments

--- UserOrganisation/app/src/main/java/com/app/userorganisation/MainActivity.java
+++ UserOrganisation/app/src/main/java/com/app/userorganisation/MainActivity.java
@@ -1,14 +1,86 @@
 package com.app.userorganisation;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.drawerlayout.widget.DrawerLayout;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.NavigationUI;
 
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.navigation.NavigationView;
 
+import java.util.Objects;
+
+public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
+    private BottomNavigationView bottomNavigationView;
+    private NavController navController;
+    private DrawerLayout drawerLayout;
+    private ActionBarDrawerToggle toggle;
+    private NavigationView navigationView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        bottomNavigationView = findViewById(R.id.bottomNavigationView);
+        navController = Navigation.findNavController(this, R.id.frame_layout);
+
+        drawerLayout = findViewById(R.id.drawerLayout);
+        navigationView = findViewById(R.id.navigation_view);
+
+        drawerLayout.addDrawerListener(toggle);
+        toggle = new ActionBarDrawerToggle(this , drawerLayout, R.string.start , R.string.close);
+        toggle.syncState();
+        try {
+            Objects.requireNonNull(getSupportActionBar()).setDisplayHomeAsUpEnabled(true);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        navigationView.setNavigationItemSelectedListener(this);
+        NavigationUI.setupWithNavController(bottomNavigationView, navController);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        if(toggle.onOptionsItemSelected(item)){
+            return true;
+        }
+        return true;
+    }
+
+    //    onNavigationItemSelected
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()){
+            case R.id.navigation_developer:
+                Toast.makeText(MainActivity.this, "Developer", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.navigation_video:
+                Toast.makeText(MainActivity.this, "Video Lectures", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.navigation_rate:
+                Toast.makeText(MainActivity.this, "Rate", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.navigation_ebook:
+                Toast.makeText(MainActivity.this, "Ebook", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.navigation_theme:
+                Toast.makeText(MainActivity.this, "Themes", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.navigation_website:
+                Toast.makeText(MainActivity.this, "Website", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.navigation_share:
+                Toast.makeText(MainActivity.this, "Share", Toast.LENGTH_SHORT).show();
+                break;
+
+        }
+
+        return true;
     }
 }
