df8ea66d98d7431150a410438a06a4348767063c
Priyam Prakash <priyamprakash99@gmail.com>
2021-11-08 21:44:45 +0530

    Navigation View

--- Organisation User/app/src/main/java/com/app/organisationuser/MainActivity.java
+++ Organisation User/app/src/main/java/com/app/organisationuser/MainActivity.java
@@ -1,14 +1,25 @@
 package com.app.organisationuser;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.NavigationUI;
 
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
+public class MainActivity extends AppCompatActivity {
+    private BottomNavigationView bottomNavigationView;
+    private NavController navController;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        bottomNavigationView = findViewById(R.id.bottomNavigationView);
+        navController = Navigation.findNavController(this, R.id.frame_layout);
+
+        NavigationUI.setupWithNavController(bottomNavigationView, navController);
+        
     }
 }
