3c510bc10d8c8dbcf376a07c61316935bd550fd1
Priyam Prakash <priyamprakash99@gmail.com>
2021-11-26 22:05:08 +0530

    New BranchAdapter

--- UserOrganisation/app/src/main/java/com/app/userorganisation/ui/about/AboutFragment.java
+++ UserOrganisation/app/src/main/java/com/app/userorganisation/ui/about/AboutFragment.java
@@ -3,6 +3,8 @@ package com.app.userorganisation.ui.about;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
+import androidx.viewpager.widget.PagerAdapter;
+import androidx.viewpager.widget.ViewPager;
 
 import android.view.LayoutInflater;
 import android.view.View;
@@ -10,13 +12,26 @@ import android.view.ViewGroup;
 
 import com.app.userorganisation.R;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class AboutFragment extends Fragment {
+    private ViewPager viewPager;
+    private BranchAdapter adapter;
+    private List<BranchModel> list;
 
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_about, container, false);
+        View view =  inflater.inflate(R.layout.fragment_about, container, false);
+        list = new ArrayList<>();
+        list.add(new BranchModel(R.drawable.ic_label , "Computer Science" , "Description of CS"));
+        list.add(new BranchModel(R.drawable.ic_label , "Mechanical Engineering" , "Description of Mechanical"));
+
+        adapter = new BranchAdapter(getContext() , list);
+
+        return view;
     }
 }
