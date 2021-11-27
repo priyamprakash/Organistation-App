4de70622fe8d577c5689e067a558fe27e66750d1
Priyam Prakash <priyamprakash99@gmail.com>
2021-11-27 22:49:17 +0530

    Minor bug fixed

--- UserOrganisation/app/src/main/java/com/app/userorganisation/ui/about/AboutFragment.java
+++ UserOrganisation/app/src/main/java/com/app/userorganisation/ui/about/AboutFragment.java
@@ -9,8 +9,10 @@ import androidx.viewpager.widget.ViewPager;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 
 import com.app.userorganisation.R;
+import com.bumptech.glide.Glide;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -31,6 +33,12 @@ public class AboutFragment extends Fragment {
         list.add(new BranchModel(R.drawable.ic_label , "Mechanical Engineering" , "Description of Mechanical"));
 
         adapter = new BranchAdapter(getContext() , list);
+        viewPager = view.findViewById(R.id.viewPager);
+        viewPager.setAdapter(adapter);
+        ImageView imageView = view.findViewById(R.id.college_img);
+        Glide.with(getContext())
+                .load("https://firebasestorage.googleapis.com/v0/b/organisation-app-3d8a5.appspot.com/o/mitmuzz.jpg?alt=media&token=cb93b50a-397a-4845-90e4-5a3361823ee7")
+                .into(imageView);
 
         return view;
     }
